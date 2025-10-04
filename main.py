439b068a7c196882736a8907985f81e3dcd53d8e
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-10-04 12:07:04 +0300

    Create an empty list and a While with prints

---
+++ main.py
@@ -0,0 +1,12 @@
+tasks = []
+
+while True:
+    print("1.Add Task")
+    print("2.Remove Task")
+    print("1.Show Task")
+    print("1.Quit")
+    choice = input("Please Enter Your Choice: ")
+
+
+
+
