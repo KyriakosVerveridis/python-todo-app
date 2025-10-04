68daa0b36292f5766b6d169aad3599dce69c01b9
KyriakosVerveridis <ververidiskyriakos@gmail.com>
2025-10-04 14:30:16 +0300

    Create the main loop of the app

--- main.py
+++ main.py
@@ -3,10 +3,26 @@ tasks = []
 while True:
     print("1.Add Task")
     print("2.Remove Task")
-    print("1.Show Task")
-    print("1.Quit")
+    print("3.Show Task")
+    print("4.Quit")
     choice = input("Please Enter Your Choice: ")
 
+    if choice.startswith("1"):
+        task = input("Enter a task: ")
+        tasks.append(task)
+    elif choice.startswith("2"):
+        task = input("Enter a task: ")
+        if task in tasks:
+            tasks.remove(task)
+        else:
+            print("Its not in tasks list.")
+    elif choice.startswith("3"):
+        print(tasks)
+    elif choice.startswith("4"):
+        print("Goodbye 👋")
+        break
+    else:
+        print("Invalid choice.")
 
 
 
